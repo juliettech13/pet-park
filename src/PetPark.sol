e759aff8566780d77199a05abefef1dd09e5493d
Juliette Chevalier <juliettech.1393@gmail.com>
2023-08-04 13:07:13 -0400

    implementation and tests

--- src/PetPark.sol
+++ src/PetPark.sol
@@ -1,6 +1,106 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
-contract PetPark {
-    
+import "openzeppelin-contracts/contracts/access/Ownable.sol";
+import "forge-std/console.sol";
+
+contract PetPark is Ownable {
+    enum AnimalType {
+        None,
+        Fish,
+        Cat,
+        Dog,
+        Rabbit,
+        Parrot
+    }
+
+    enum Gender {
+        NotYetDefined,
+        Female,
+        Male
+    }
+
+    mapping(AnimalType => uint256) public animalCounts;
+    mapping(address => AnimalType) public borrowedAnimals;
+    mapping(address => uint256) public ages;
+    mapping(address => Gender) public genders;
+
+    event Added(AnimalType animalType, uint256 count);
+    event Returned(AnimalType animalType);
+    event Borrowed(AnimalType animalType);
+
+    modifier canGiveBackAnimal() {
+       require(uint(borrowedAnimals[msg.sender]) > 0, "No borrowed pets");
+        _;
+    }
+
+    modifier cantAddNone(AnimalType _animalType) {
+        require(uint256(_animalType) > 0, "Invalid animal");
+        _;
+    }
+
+    modifier ageZero(uint256 _age) {
+        require(_age > 0, "Cant borrow at age 0");
+        _;
+    }
+
+    /// @notice Adds an animal count to the shelter capacity
+    /// @param _animalType The type of animal being added
+    /// @param _count The amount of animals being made available
+    function add(AnimalType _animalType, uint256 _count)
+      public
+      onlyOwner
+      cantAddNone(_animalType)
+    {
+        animalCounts[AnimalType(_animalType)] += _count;
+        emit Added(AnimalType(_animalType), _count);
+    }
+
+    /// @notice Borrows an animal from the shelter
+    /// @param _age The age of the user borrowing the animal
+    /// @param _gender The gender of the user borrowing the animal
+    /// @param _animalType The type of animal being borrowed
+    function borrow(uint256 _age, Gender _gender, AnimalType _animalType)
+      public
+      ageZero(_age)
+    {
+        if (ages[msg.sender] != 0) {
+            require(ages[msg.sender] == _age, "Invalid Age");
+        } else {
+            ages[msg.sender] = _age;
+        }
+
+        if (uint256(genders[msg.sender]) != 0) {
+            require(genders[msg.sender] == _gender, "Invalid Gender");
+        } else {
+            genders[msg.sender] = _gender;
+        }
+
+        require(uint(borrowedAnimals[msg.sender]) == 0, "Already adopted a pet");
+
+        if (_animalType == AnimalType.None) {
+            revert("Invalid animal type");
+        } else if (_gender == Gender.Male) {
+            require(_animalType == AnimalType.Dog || _animalType == AnimalType.Fish, "Invalid animal for men");
+        } else if (_gender == Gender.Female && _animalType == AnimalType.Cat) {
+            require(_age > 40, "Invalid animal for women under 40");
+        }
+
+        require(animalCounts[AnimalType(_animalType)] > 0, "Selected animal not available");
+
+        animalCounts[AnimalType(_animalType)] -= 1;
+        borrowedAnimals[msg.sender] = AnimalType(_animalType);
+        emit Borrowed(AnimalType(_animalType));
+    }
+
+    /// @notice Gives back an animal to the shelter
+    function giveBackAnimal()
+      public
+      canGiveBackAnimal
+    {
+        AnimalType borrowedAnimal = borrowedAnimals[msg.sender];
+        borrowedAnimals[msg.sender] = AnimalType.None;
+        animalCounts[borrowedAnimal] += 1;
+        emit Returned(AnimalType(borrowedAnimal));
+    }
 }
--- test/PetPark.t.sol
+++ test/PetPark.t.sol
@@ -4,10 +4,9 @@ pragma solidity ^0.8.13;
 import "forge-std/Test.sol";
 import "../src/PetPark.sol";
 
-
 contract PetParkTest is Test, PetPark {
     PetPark petPark;
-    
+
     address testOwnerAccount;
 
     address testPrimaryAccount;
@@ -26,8 +25,10 @@ contract PetParkTest is Test, PetPark {
     }
 
     function testCannotAddAnimalWhenNonOwner() public {
-        // 1. Complete this test and remove the assert line below
-        assert(false);
+        vm.expectRevert("Ownable: caller is not the owner");
+
+        vm.prank(testPrimaryAccount);
+        petPark.add(AnimalType.Fish, 2);
     }
 
     function testCannotAddInvalidAnimal() public {
@@ -43,8 +44,9 @@ contract PetParkTest is Test, PetPark {
     }
 
     function testCannotBorrowWhenAgeZero() public {
-        // 2. Complete this test and remove the assert line below
-        assert(false);
+        vm.expectRevert("Cant borrow at age 0");
+
+        petPark.borrow(0, Gender.Male, AnimalType.None);
     }
 
     function testCannotBorrowUnavailableAnimal() public {
@@ -94,7 +96,7 @@ contract PetParkTest is Test, PetPark {
         vm.prank(testPrimaryAccount);
         petPark.borrow(24, Gender.Male, AnimalType.Fish);
 
-		vm.expectRevert("Already adopted a pet");
+        vm.expectRevert("Already adopted a pet");
         vm.prank(testPrimaryAccount);
         petPark.borrow(24, Gender.Male, AnimalType.Fish);
 
@@ -109,11 +111,11 @@ contract PetParkTest is Test, PetPark {
         vm.prank(testPrimaryAccount);
         petPark.borrow(24, Gender.Male, AnimalType.Fish);
 
-		vm.expectRevert("Invalid Age");
+        vm.expectRevert("Invalid Age");
         vm.prank(testPrimaryAccount);
         petPark.borrow(23, Gender.Male, AnimalType.Fish);
 
-		vm.expectRevert("Invalid Gender");
+        vm.expectRevert("Invalid Gender");
         vm.prank(testPrimaryAccount);
         petPark.borrow(24, Gender.Female, AnimalType.Fish);
     }
@@ -127,8 +129,12 @@ contract PetParkTest is Test, PetPark {
     }
 
     function testBorrowCountDecrement() public {
-        // 3. Complete this test and remove the assert line below
-        assert(false);
+        petPark.add(AnimalType.Fish, 5);
+        uint256 fishAvailable = petPark.animalCounts(AnimalType.Fish);
+
+        vm.prank(testPrimaryAccount);
+        petPark.borrow(24, Gender.Male, AnimalType.Fish);
+        assert(fishAvailable - 1 == petPark.animalCounts(AnimalType.Fish));
     }
 
     function testCannotGiveBack() public {
@@ -140,11 +146,11 @@ contract PetParkTest is Test, PetPark {
         petPark.add(AnimalType.Fish, 5);
 
         petPark.borrow(24, Gender.Male, AnimalType.Fish);
-        uint reducedPetCount = petPark.animalCounts(AnimalType.Fish);
+        uint256 reducedPetCount = petPark.animalCounts(AnimalType.Fish);
 
         petPark.giveBackAnimal();
-        uint currentPetCount = petPark.animalCounts(AnimalType.Fish);
+        uint256 currentPetCount = petPark.animalCounts(AnimalType.Fish);
 
-		assertEq(reducedPetCount, currentPetCount - 1);
+        assertEq(reducedPetCount, currentPetCount - 1);
     }
 }
